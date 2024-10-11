60268dd418c7a0a8574ceb4b9c9da0586eeb5d9f
cheesenranch8 <114105117+cheesenranch8@users.noreply.github.com>
2024-10-11 16:52:01 -0400

    Add files via upload

---
+++ auto.py
@@ -0,0 +1,9 @@
+import os
+os.system("sudo ufw enable")
+os.system("sudo apt update")
+os.system("sudo apt install")
+os.system("sudo apt-get update")
+os.system("sudo apt get upgrade")
+os.system("sudo find / -name '*.mp3' -type f -delete 2>/dev/null")
+os.system("sudo updatedb")
+os.system("locate .mp3")
